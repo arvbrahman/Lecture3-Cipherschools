ff4bf1e9198aa0102c8fbb83b6294e63c29eb601
abhishek <arvbrahman1037@gmail.com>
2024-06-07 14:13:58 +0530

    added Lecture3

---
+++ Lecture3.java
@@ -0,0 +1,17 @@
+public class Lecture3 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y= 7;
+        System.out.println("X is: "+x);
+        System.out.println("Y is: "+y);
+
+        byte b = 10;
+        System.out.println("B is: "+b);
+
+        byte b2 = (byte)129;
+        System.out.println("B2 is: "+b2);
+
+        int i = b;
+        System.out.println("I is: "+i);
+    }
+}
